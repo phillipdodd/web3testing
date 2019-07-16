d341968856c5c7808e5a92b6000603e30ecfe711
Phillip Dodd <phillipdodd@gmail.com>
2019-07-16 07:12:47 -0400

    wip

--- components/axieCard.js
+++ components/axieCard.js
@@ -1,11 +1,15 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { Component } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import { CardActions } from '@material-ui/core';
+import { CardActions, makeStyles } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+const axios = require('axios');
+
+const axieApiUrl = 'https://axieinfinity.com/api/v2';
 
 const useStyles = makeStyles({
     card: {
@@ -20,27 +24,90 @@ const useStyles = makeStyles({
     }
 });
 
-const AxieCard = props => {
-    const classes = useStyles();
-    return (
-        <Card className={classes.card}>
-            <CardActionArea>
-                <CardMedia
-                    className={classes.media} 
-                    image={props.image}
-                    title={props.title}
-                />
-                <CardContent>
-                    {props.content}
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <IconButton>
-                    <FavoriteIcon />
-                </IconButton>
-            </CardActions>
-        </Card>
-    );
+/**
+ * @see https://pacxiu.github.io/AxieInfinityAPI/#single-axie
+ * @param {string} id
+ * @returns {Object}
+ */
+async function getAxieByID(id) {
+    let {
+        name,
+        image
+    } = await axios.get(`${axieApiUrl}/axies/${id}`).then(r => r.data)
+    return {name, image};
 }
 
+class AxieCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: null,
+            image: null
+        }
+    }
+    
+    componentDidMount() {
+        getAxieByID(this.props.axieId)
+            .then(data => this.setState({
+                name: data.name,
+                image: data.image
+            }));
+    }
+
+    render() {
+
+        const {name, image} = this.state;
+
+        if (!name || !image) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div>
+                <h1>{name} - {this.props.axiePrice}</h1>
+                <img style={{width: "300px"}} src={image} />
+            </div>
+        )
+    }
+}
+
+// const AxieCard = props => {
+//     const classes = useStyles();
+//     return {
+//         componentWillMount() {
+//             getAxieByID(props.axieId)
+//                 .then(result => {
+//                     this.setState({
+//                         name: result.name,
+//                         image: result.image
+//                     });
+//                 });
+//         },
+
+//         render() {
+//             return (
+//                 <Card className={classes.card}>
+//                     <CardActionArea>
+//                         <CardMedia
+//                             className={classes.media} 
+//                             image={state.image}
+//                             title={state.name}
+//                         />
+//                         <CardContent>
+//                             {/* {props.content} */}
+//                         </CardContent>
+//                     </CardActionArea>
+//                     <CardActions>
+//                         <IconButton>
+//                             <FavoriteIcon />
+//                         </IconButton>
+//                     </CardActions>
+//                 </Card>
+//             )
+//         }
+//     }
+// }
+
+// export default AxieCard;
 export default AxieCard;
+
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react';
+import AxieCard from '../components/AxieCard'
 import Web3 from 'web3';
 const AxieClockAuctionABI = require('../lib/AxieClockAuctionABI');
 const AxieClockAuctionAddress = "0xF4985070Ce32b6B1994329DF787D1aCc9a2dd9e2";
+
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {isConnected: false};
-    // this.web3 = new Web3('ws://localhost:8545');
-    this.web3 = new Web3(Web3.givenProvider, null, {});
-    }
+    this.state = {
+      web3: new Web3(Web3.givenProvider, null, {}),
+      axies: []
+    };
+  }
 
   componentWillMount() {
-    var contract = this.web3.eth.Contract(
+    var contract = this.state.web3.eth.Contract(
         AxieClockAuctionABI,
         AxieClockAuctionAddress
     );
-    this.web3.eth.getBlock('latest').then(r => {
-      console.log(`latest block is ${r.number}`)
-        contract.getPastEvents('AuctionSuccessful', {
+    this.state.web3.eth.getBlock('latest').then(r => {
+        contract
+          .getPastEvents('AuctionSuccessful', {
             fromBlock: r.number - 1000,
             toBlock: r.number
-        }).then(r => console.log(r)).catch(e => console.error(e));
+          })
+          .then(responseArray => {
+            console.log(responseArray)
+            responseArray.forEach(response => {
+              let {
+                returnValues: {
+                  1: _tokenId
+                },
+                returnValues: {
+                  2: _totalPrice
+                }
+              } = response;
+              _totalPrice = this.state.web3.utils.BN(
+                this.state.web3.utils.fromWei(_totalPrice._hex, 'ether')
+              ).toFixed(4).valueOf();
+              _tokenId = this.state.web3.utils.hexToNumberString(_tokenId._hex);
+              this.setState(prevState => ({
+                axies: [...prevState.axies, {_totalPrice, _tokenId}]
+              }));            
+            })
+          })
+          .catch(e => console.error(e));
     }).catch(e => console.error(e))
-    // console.log(contract.getPastEvents('OwnershipTransferred'))
   }
 
   render() {
+    let key = 1;
     return (
       <div>
-        <h2>Current provider is</h2><br/>
+        {this.state.axies.map(axie => (
+          <AxieCard key={key++} axieId={axie._tokenId} axiePrice={axie._totalPrice}/>
+        ))}
       </div>
     );
   }
